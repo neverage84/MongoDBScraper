f4d08a599278f48342368acafee0fc3e94260c00
Noelle Everage <neverage84@yahoo.com>
2020-01-31 17:55:11 -0800

    Added controller to handle page render in handlebars

---
+++ controller/scrape_controller.js
@@ -0,0 +1,60 @@
+var express = require("express");
+
+var router = express.Router();
+// Import the model (burger.js) to use its database functions.
+// var burger = require("../models/burger.js");
+
+
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    res.render("index");
+    });
+
+//   router.get("/api/burgers", function(req, res) {
+//     burger.all(function(data) {
+//       var hbsObject = {
+//         burger: data
+//       };
+//       console.log(hbsObject);
+//       res.render("index", hbsObject);
+//     });
+//   });
+  router.post("/api/burgers", function(req, res) {
+    var NewName = " ' " + req.body.name + " ' ";
+    
+    burger.create(NewName, function(result) {
+      // Send back the ID of the new quote
+    //     console.log("Hello");
+      console.log(result);
+      res.redirect("/")
+    //   res.json({ id: result.insertId });
+    });
+  });
+
+
+
+
+router.put("/burgers/:id", function(req, res) {
+    console.log(req.params.id);
+    var condition = "id=" + req.params.id;
+
+    burger.update(
+      condition,
+      function(result) {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+  
+      })
+  });
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
+
+
--- server.js
+++ server.js
@@ -1,39 +1,27 @@
+var exphbs = require("express-handlebars");
 var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
 
 
-// Require all models
-// var db = require("./models");
+var PORT = process.env.PORT || 3000;
 
-var PORT = 3000;
-
-// Initialize Express
 var app = express();
 
-// Configure middleware
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + '/public'));
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
-
-// Connect to the Mongo DB
-// mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
-
-// Routes
 
 
-// Start the server
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controller/scrape_controller.js");
+
+app.use(routes);
+
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
-  });
+  console.log("App now listening at localhost:" + PORT);
+});
