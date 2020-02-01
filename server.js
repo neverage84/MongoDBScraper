da3898fb07bae37af11931daf2a8a527094eb29f
Noelle Everage <neverage84@yahoo.com>
2020-01-31 18:01:51 -0800

    Added back in the logger, mongoose, axios, and cheerio import

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 var exphbs = require("express-handlebars");
 var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
 
 
 var PORT = process.env.PORT || 3000;
 
+// Initialize Express
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + '/public'));
 
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
